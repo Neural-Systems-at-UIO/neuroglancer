6027b247439b1373a43150b6158b4865062a8d61
Tomaz Vieira <tom.fogaca@gmail.com>
2022-09-10 00:00:43 +0200

    Refreshes ebrains token on 401

--- src/neuroglancer/util/fetch_wrapper.ts
+++ src/neuroglancer/util/fetch_wrapper.ts
@@ -14,33 +14,37 @@ import { Url } from "./url";
 var ebrains_user_access_token: string | undefined = undefined
 
 const __origFetch = self.fetch;
-export const hijackedFetch = async (input: RequestInfo, init?: RequestInit) => {
+
+const fetchUserToken = async (): Promise<string> => {
+    const token_response = await __origFetch("https://app.ilastik.org/api/get_ebrains_token", {method: "POST"})
+    if(!token_response.ok){
+        throw TypeError(await token_response.text())
+    }
+    return (await token_response.json())["ebrains_user_access_token"]
+}
+
+export const hijackedFetch = async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
     const url = Url.parse(typeof input === "string" ? input : input.destination)
     if(!url.raw.startsWith("https://data-proxy.ebrains.eu/api/")){
         return __origFetch(input, init);
     }
 
     if(ebrains_user_access_token === undefined){
-        const token_response = await fetch("https://app.ilastik.org/api/get_ebrains_token", {method: "POST"})
-        if(!token_response.ok){
-            throw TypeError(await token_response.text())
-        }
-        ebrains_user_access_token = (await token_response.json()).ebrains_user_access_token
+        ebrains_user_access_token = await fetchUserToken()
     }
 
-    const authHeaderName = "Authorization"
-    const authHeaderValue = `Bearer ${ebrains_user_access_token}`
     let headers: HeadersInit | undefined = init === undefined ? undefined :  init.headers
-    let fixedHeaders: HeadersInit
-    if(headers === undefined){
-        fixedHeaders = {[authHeaderName]: authHeaderValue}
-    }else if(Array.isArray(headers)){
-        fixedHeaders = [...headers, [authHeaderName, authHeaderValue]]
+    let fixedHeaders: {[key:string]: string} = {}
+    if(Array.isArray(headers)){
+        headers.forEach(header => fixedHeaders[header[0]] = header[1])
     }else if(headers instanceof Headers){
-        fixedHeaders = [...headers.entries(), [authHeaderName, authHeaderValue]]
-    }else{
-        fixedHeaders = {...headers, [authHeaderName]: authHeaderValue}
+        for(let header of headers.entries()){
+            fixedHeaders[header[0]] = header[1]
+        }
+    }else if(headers !== undefined){
+        fixedHeaders = {...fixedHeaders, ...headers}
     }
+    fixedHeaders["Authorization"] = `Bearer ${ebrains_user_access_token}`
 
     let http_method = (init && init.method && init.method.toUpperCase()) || "GET";
 
@@ -51,19 +55,23 @@ export const hijackedFetch = async (input: RequestInfo, init?: RequestInit) => {
         fixedInput = input
     }
 
-    const responsePromise = __origFetch(fixedInput, {...init, headers: fixedHeaders});
-
-    if(url.path.name == "stat"){
-        return responsePromise
+    let response = await __origFetch(fixedInput, {...init, headers: fixedHeaders});
+    if(response.status == 401){
+        ebrains_user_access_token = await fetchUserToken();
+        fixedHeaders["Authorization"] = `Bearer ${ebrains_user_access_token}`
+        response = await __origFetch(fixedInput, {...init, headers: fixedHeaders});
     }
 
-    const response = await responsePromise
+    if(url.path.name == "stat"){
+        return response
+    }
+
     if(!response.ok){
-        return responsePromise
+        return response
     }
     const response_payload = await response.json();
     const cscsObjectUrl = response_payload["url"]
-    return await fetch(cscsObjectUrl)
+    return await __origFetch(cscsObjectUrl)
 };
 
 self.fetch = hijackedFetch
